04fba838d04e14dea3b7dfa3328309fb1cdc736b
Shrit Bansal <shritbansal5@gmail.com>
2025-08-13 16:07:58 +0530

    Add basic Express server

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
